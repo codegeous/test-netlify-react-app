2caf9e926fffa80ed0be107868656841325aa9f9
Prem Kumar <premkumarssp@gmail.com>
2020-06-23 19:44:34 +0400

    Updated channel URL

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to D'coders Youtube Channel
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Welcome to D'coders Youtube Channel</p>
+                <a
+                    className="App-link"
+                    href="https://www.youtube.com/channel/UCdItDI6oTgPW7l9WOJI7ItA/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Channel Page
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
